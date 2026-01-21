8c872264e214e6f3d2881b47f47d1d9f459af7c9
KabirKhanuja <kabirkhanuja@gmail.com>
2026-01-22 01:05:52 +0530

    resuable button for all lg actions

---
+++ lib/widgets/action_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ActionButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const ActionButton({super.key, required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 14),
+        textStyle: const TextStyle(fontSize: 16),
+      ),
+      child: Text(label),
+    );
+  }
+}

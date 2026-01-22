9b0a062573750627f4ef95c2da5eb78c46d29fe7
KabirKhanuja <kabirkhanuja@gmail.com>
2026-01-22 20:26:50 +0530

    settings for asking ip, username, password, etc

---
+++ lib/settings/lg_connection_config.dart
@@ -0,0 +1,13 @@
+class LgConnectionConfig {
+  final String host;
+  final String username;
+  final int port;
+  final String password;
+
+  const LgConnectionConfig({
+    required this.host,
+    required this.username,
+    required this.port,
+    required this.password,
+  });
+}
